ae0fc878ab4684256c780c5d34525f9e408b8f0e
wbauknecht80 <wbauknecht80@gmail.com>
2022-09-15 07:44:22 -0500

    Create add, subtract, multiply and divide functions. Tested to makes sure CSS was properly connected (hence the 'test' text and div container). Created div space for the actual calculator.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,38 @@
 console.log('This is a test!')
+
+function add(a, b) {
+    var c = a + b;
+    return c;
+}
+
+function subtract(a, b){
+    var c = a - b;
+    return c;
+}
+
+function multiply(a, b){
+    var c = a * b;
+    return c;
+}
+
+function divide(a,b){
+    var c = a / b;
+    return c;
+}
+
+function operate(operator, a, b) {
+    var operator = prompt("Add, Subtract, Multiply, or Divide?");
+    var x = prompt("First number?");
+    var y = prompt("Second number?");
+    var a = parseInt(x);
+    var b = parseInt(y);
+    if (operator == "Add"){
+        return add(a, b);
+    } else if (operator == "Subtract") {
+        return subtract(a, b);
+    } else if (operator == "Multiply") {
+        return multiply(a, b);
+    } else if (operator == "Divide") {
+        return divide(a, b);
+    }
+}
