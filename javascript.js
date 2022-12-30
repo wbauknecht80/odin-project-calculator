f882f36b88a29e06cd402cf22dfb2dbbcee7f0e9
wbauknecht80 <wbauknecht80@gmail.com>
2022-12-30 14:07:03 -0600

    Make numbers print to firstVar box Need to fix the updateOperator function. Not sure why it is not working properly.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 const calcButtons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
-let displayNumber = '';
-
 const numButtons = document.getElementsByClassName('number');
 const operationButtons = document.getElementsByClassName('operator');
 const equalsButton = document.getElementById('equal')
@@ -10,9 +8,6 @@ const deleteButton = document.getElementById('delete')
 const operationDisplay = document.getElementsByClassName('operationDisplay');
 const resultantDisplay = document.getElementsByClassName('resultantDisplay');
 
-function appendNumber(number) {
-
-}
 
 function add(a, b) {
     var c = a + b;
@@ -35,16 +30,41 @@ function divide(a,b){
 }
 
 function exponent(a,b){
-    var c = Math.pow(a, b)
-    return c
+    var c = Math.pow(a, b);
+    return c;
+}
+
+document.querySelectorAll('.number').forEach(item => {
+    item.addEventListener('click', event => {
+        updateVars(item.textContent);
+    })
+})
+
+const firstVarSpan = document.getElementById('firstVar');
+const secondVarSpan = document.getElementById('secondVar');
+const operatorSpan = document.getElementById('operatorSign');
+
+function updateVars(value){
+    if (operatorSpan.textContent == '') {
+        firstVarSpan.textContent += value;
+    } else {
+        secondVarSpan.textContent += value;
+    }
+} 
+        
+document.querySelectorAll('.operator').forEach(item => {
+    item.addEventListener('click', event => {
+        updateOperator(item.textContent);
+    })
+})
+
+function updateOperator(value){
+    if (operatorSpan.textContent == '' && firstVarSpan.textContent != '') {
+        operatorSpan.textContent += value; 
+    }
 }
 
 function operate(operator, a, b) {
-    var operator = prompt("Add, Subtract, Multiply, Divide, or Exponent?");
-    var x = prompt("First number?");
-    var y = prompt("Second number?");
-    var a = parseInt(x);
-    var b = parseInt(y);
     if (operator == "Add"){
         return add(a, b);
     } else if (operator == "Subtract") {
@@ -57,9 +77,3 @@ function operate(operator, a, b) {
         return exponent(a, b)
     }
 }
-
-let fillData = () => {
-    let ele = document.getElementById('display');
-    let node = operate()
-}
-
